f64f7c56c34c6ed8fcfb7964c47ed23cf38a5dc1
ybonnel <ybonnel@gmail.com>
2012-10-19 15:28:50 +0200

    Organize import

--- src/fr/ybo/transportsrennes/visu/PositionBusServlet.java
+++ src/fr/ybo/transportsrennes/visu/PositionBusServlet.java
@@ -16,7 +16,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
